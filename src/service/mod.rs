41c9ad3726d6736b9df48823bbf9e5eca7ac2c4f
Tunglies <coffee@soe.icu>
2025-02-16 20:10:59 +0800

    chore: downgrade nix version

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -159,7 +159,7 @@ pub fn main() -> Result<()> {
 #[cfg(not(windows))]
 pub fn main() {
     #[cfg(target_os = "linux")]
-    web::init_signal_handler();
+    core::init_signal_handler();
     if let Ok(rt) = Runtime::new() {
         rt.block_on(async {
             let _ = run_service().await;
